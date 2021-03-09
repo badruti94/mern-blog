11f387a40d73b4a0dc8348d57dd0c51f79ca1d59
Badru <badruakfm@gmail.com>
2021-03-09 14:01:40 +0700

    update

---
+++ src/components/atoms/Input/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Input = () => {
+    return (
+        <div>
+            <p>label input</p>
+            <input type="text" placeholder="form input" />
+        </div>
+    )
+}
+
+export default Input
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { RegisterBg } from '../../assets'
+import './register.scss'
 
 const Register = () => {
     return (
-        <div>
-            <p>Register Page</p>
+        <div className="main-page">
+            <div className="left" >
+                <img src={RegisterBg} className="bg-image" alt="alt" />
+            </div>
+            <div className="right" >Form Register</div>
         </div>
     )
 }
