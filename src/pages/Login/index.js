d19db5f7195744c7f59a4b3537c9a16d83fce4e2
Badru <badruakfm@gmail.com>
2021-03-10 10:53:23 +0700

    07 - Konsep SCSS Global & Slicing Halaman Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { LoginBg } from '../../assets'
+import {Button, Gap, Input, Link} from '../../components'
 
 const Login = () => {
     return (
-        <div>
-           <p>Login Page</p> 
+        <div className="main-page">
+            <div className="left" >
+                <img src={LoginBg} className="bg-image" alt="imageBg" />
+            </div>
+            <div className="right" >
+                <p className="title" >Login</p>
+                <Input label="Email" placeholder="Email" />
+                <Gap height={18} />
+                <Input label="Password" placeholder="Password" />
+                <Gap height={50} />
+                <Button title="login" />
+                <Link title="Belum punya akun, silahkan daftar" />
+            </div>
         </div>
     )
 }
