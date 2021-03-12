649b91d7eeae025a156818b4162e48aec42d3350
Badru <badruakfm@gmail.com>
2021-03-12 21:38:04 +0700

    49 - Implementasi Menambahkan Postingan Baru dan Upload Image

--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,20 +1,55 @@
-import React from 'react'
+import Axios from 'axios';
+import React, { useState } from 'react'
 import {Input, Button, Upload, TextArea, Gap, Link} from '../../components'
 import './createBlog.scss'
 import {useHistory} from 'react-router-dom'
 
 const CreateBlog = () => {
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [image, setImage] = useState('')
+    const [imagePreview, setImagePreview] = useState(null)
+
+    const onSubmit = () => {
+        console.log(title);
+        console.log(body);
+        console.log(image);
+
+        const data = new FormData()
+        data.append('title', title)
+        data.append('body', body)
+        data.append('image', image)
+
+        Axios.post('http://localhost:4000/v1/blog/post', data, {
+            headers: {
+                'content-type' : 'multipart/form-data'
+            }
+        })
+        .then(res => {
+            console.log('post success: ', res);
+        })
+        .catch(err => {
+            console.log('err: ', err);
+        })
+    }
+
+    const onImageUpload = e => {
+        const file = e.target.files[0]
+        setImage(file)
+        setImagePreview(URL.createObjectURL(file))
+    }
+
     const history = useHistory()
     return (
         <div className="blog-post" >
             <Link title="kembali" onClick={()=>{history.push('/')}} />
             <p className="title" >Create New Blog Post</p>
-            <Input label="Post Title" />
-            <Upload />
-            <TextArea />
+            <Input label="Post Title" value={title} onChange={e => setTitle(e.target.value)} />
+            <Upload onChange={e => onImageUpload(e) } img={imagePreview} />
+            <TextArea value={body} onChange={e => setBody(e.target.value)} />
             <Gap height={20} />
             <div className="button-action" >
-            <Button title="Save" />
+            <Button title="Save" onClick={onSubmit} />
             </div>
 
         </div>
