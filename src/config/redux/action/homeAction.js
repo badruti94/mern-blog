f7cb6debb84595b9c1a2ac6997f5d7c781f578c5
Badru <badruakfm@gmail.com>
2021-03-12 20:29:23 +0700

    47 - Memahami Action Creator dan Async Store dengan redux thunk

---
+++ src/config/redux/action/homeAction.js
@@ -0,0 +1,20 @@
+import Axios from 'axios'
+export const setDataBlog = () => {
+    return (dispatch) => {
+        Axios.get('http://localhost:4000/v1/blog/posts?page=2&perPage=2')
+            .then(result => {
+                const responseAPI = result.data
+
+                console.log(responseAPI);
+
+                dispatch({
+                    type: 'UPDATE_DATA_BLOG',
+                    payload: responseAPI.data
+                })
+
+            })
+            .catch(err => {
+                console.log('error : ', err);
+            })
+    }
+}
