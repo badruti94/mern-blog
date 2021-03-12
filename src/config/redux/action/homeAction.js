2765dbc49faa70e6540a6de7e12742176100a925
Badru <badruakfm@gmail.com>
2021-03-12 20:51:38 +0700

    48 - Implementasi Pagination di halaman homepage

--- src/config/redux/action/homeAction.js
+++ src/config/redux/action/homeAction.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
-export const setDataBlog = () => {
+export const setDataBlog = (page) => {
     return (dispatch) => {
-        Axios.get('http://localhost:4000/v1/blog/posts?page=2&perPage=2')
+        Axios.get(`http://localhost:4000/v1/blog/posts?page=${page}&perPage=8`)
             .then(result => {
                 const responseAPI = result.data
 
@@ -10,6 +10,13 @@ export const setDataBlog = () => {
                 dispatch({
                     type: 'UPDATE_DATA_BLOG',
                     payload: responseAPI.data
+                })
+                dispatch({
+                    type : 'UPDATE_PAGE',
+                    payload: {
+                        currentPage: responseAPI.current_page,
+                        totalPage: Math.ceil(responseAPI.total_data / responseAPI.per_page)
+                    }
                 })
 
             })
