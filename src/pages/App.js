eaf2b797f3576f341f28ee18d67880828042a7ab
Badru <badruakfm@gmail.com>
2021-03-12 18:05:40 +0700

    44 - Setup State Management Redux

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,17 @@
 
+import { Provider } from 'react-redux';
+import store from '../config/redux/store';
 import Routes from '../config/Routes';
 import './App.css';
 
 function App() {
   console.log(Routes);
 
-  return (
-    <Routes />
+  return ( 
+    <Provider store={store} >
+      <Routes />
+    </Provider>
+    
   );
 }
 
